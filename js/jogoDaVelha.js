629f599dbddd895dd53024ad4232092bd81769a4
Danieldandrade <dan.andrade20@gmail.com>
2025-01-28 07:09:33 -0300

    2º commit

---
+++ js/jogoDaVelha.js
@@ -0,0 +1,22 @@
+let tabuleiro
+let bord
+let aviso
+let jogador
+let linha
+let coluna
+
+function Iniciar(){
+    tabuleiro = []
+}
+
+function exibir(){
+
+}
+
+function jogar(){
+
+}
+
+function Checar(){
+
+}
