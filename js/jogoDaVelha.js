f9621c1d30e9480b7065c772c63d83c1bf3c08c3
Danieldandrade <dan.andrade20@gmail.com>
2025-01-28 07:35:34 -0300

    array trimensional

--- js/jogoDaVelha.js
+++ js/jogoDaVelha.js
@@ -5,8 +5,21 @@ let jogador
 let linha
 let coluna
 
-function Iniciar(){
+function iniciar(){
     tabuleiro = []
+    board = document.getElementById('board')
+    aviso = document.getElementById('aviso')
+    jogardor = 1
+
+    for(let i = 0; i < 3; i++){
+        tabuleiro [i] = []
+        for(let j = 0; j < 3;j++ ){
+            tabuleiro[i][j] = 0
+        }
+    }
+
+
+    console.table(tabuleiro)
 }
 
 function exibir(){
